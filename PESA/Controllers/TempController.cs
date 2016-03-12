39ae5ab3a76b424372f9d4a86e86bcb1b5feae18
Serhat KOLCU <serhat.kolcu@ogr.sakarya.edu.tr>
2016-03-13 01:05:44 +0200

    Temp1

---
+++ PESA/Controllers/TempController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PESA.Controllers
+{
+    public class TempController : Controller
+    {
+        // GET: Temp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz23()
+        {
+            return View();
+        }
+    }
+}
