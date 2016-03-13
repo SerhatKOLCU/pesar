e7373f83df005ecf3ce587479b5c29381ed21292
Serhat KOLCU <serhat.kolcu@ogr.sakarya.edu.tr>
2016-03-13 13:51:46 +0200

    beninki

--- PESA/Controllers/TempController.cs
+++ PESA/Controllers/TempController.cs
@@ -14,6 +14,179 @@ namespace PESA.Controllers
             return View();
         }
 
+        public ActionResult Gorus43()
+        {
+            return View();
+        }
+        public ActionResult Gorus42()
+        {
+            return View();
+        }
+        public ActionResult Gorus41()
+        {
+            return View();
+        }
+        public ActionResult Gorus40()
+        {
+            return View();
+        }
+        public ActionResult Gorus39()
+        {
+            return View();
+        }
+        public ActionResult Gorus38()
+        {
+            return View();
+        }
+        public ActionResult Gorus37()
+        {
+            return View();
+        }
+        public ActionResult Gorus36()
+        {
+            return View();
+        }
+        public ActionResult Gorus35()
+        {
+            return View();
+        }
+        public ActionResult Gorus34()
+        {
+            return View();
+        }
+        public ActionResult Gorus33()
+        {
+            return View();
+        }
+        public ActionResult Gorus32()
+        {
+            return View();
+        }
+        public ActionResult Gorus31()
+        {
+            return View();
+        }
+        public ActionResult Gorus30()
+        {
+            return View();
+        }
+        public ActionResult Gorus29()
+        {
+            return View();
+        }
+        public ActionResult Gorus28()
+        {
+            return View();
+        }
+        public ActionResult Gorus27()
+        {
+            return View();
+        }
+        public ActionResult Gorus26()
+        {
+            return View();
+        }
+        public ActionResult Gorus25()
+        {
+            return View();
+        }
+        public ActionResult Gorus24()
+        {
+            return View();
+        }
+        public ActionResult Gorus23()
+        {
+            return View();
+        }
+        public ActionResult Gorus22()
+        {
+            return View();
+        }
+        public ActionResult Gorus21()
+        {
+            return View();
+        }
+        public ActionResult Gorus20()
+        {
+            return View();
+        }
+        public ActionResult Gorus19()
+        {
+            return View();
+        }
+        public ActionResult Gorus18()
+        {
+            return View();
+        }
+        public ActionResult Gorus17()
+        {
+            return View();
+        }
+        public ActionResult Gorus16()
+        {
+            return View();
+        }
+        public ActionResult Gorus15()
+        {
+            return View();
+        }
+        public ActionResult Gorus14()
+        {
+            return View();
+        }
+        public ActionResult Gorus13()
+        {
+            return View();
+        }
+        public ActionResult Gorus12()
+        {
+            return View();
+        }
+        public ActionResult Gorus11()
+        {
+            return View();
+        }
+        public ActionResult Gorus10()
+        {
+            return View();
+        }
+        public ActionResult Gorus9()
+        {
+            return View();
+        }
+        public ActionResult Gorus8()
+        {
+            return View();
+        }
+        public ActionResult Gorus7()
+        {
+            return View();
+        }
+        public ActionResult Gorus6()
+        {
+            return View();
+        }
+        public ActionResult Gorus5()
+        {
+            return View();
+        }
+        public ActionResult Gorus4()
+        {
+            return View();
+        }
+        public ActionResult Gorus3()
+        {
+            return View();
+        }
+        public ActionResult Gorus2()
+        {
+            return View();
+        }
+        public ActionResult Gorus1()
+        {
+            return View();
+        }
+
         public ActionResult Analiz23()
         {
             return View();
