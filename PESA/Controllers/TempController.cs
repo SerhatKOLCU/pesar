b7c5ebfc3fae8b3080e3441c53481f003079c831
Ozan Kömürcü <ozankomurcu@gmail.com>
2016-03-13 13:44:01 +0200

    ANAL

--- PESA/Controllers/TempController.cs
+++ PESA/Controllers/TempController.cs
@@ -14,9 +14,115 @@ namespace PESA.Controllers
             return View();
         }
 
-        public ActionResult Analiz23()
+        public ActionResult Analiz22()
         {
             return View();
         }
+
+        public ActionResult Analiz21()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz20()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz19()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz18()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz17()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz16()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz15()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz14()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz13()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz12()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz11()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz10()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz9()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz8()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz7()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz6()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz5()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz4()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz3()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz2()
+        {
+            return View();
+        }
+
+        public ActionResult Analiz1()
+        {
+            return View();
+        }
+
     }
 }
